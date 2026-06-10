def96ab1e21a167c618821aafbd643d062bbee21
user1411\1411 <dabinkim0225@gmail.com>
2026-06-10 14:31:10 +0900

    feat : 가전 캘린더 버튼 이미지 수정

--- Web_ui/src/pages/CalendarPage.jsx
+++ Web_ui/src/pages/CalendarPage.jsx
@@ -368,9 +368,7 @@ export default function CalendarPage({
               closeDateDetail();
             }}
           >
-            <span className="vacuum-symbol" aria-hidden="true">
-              <i />
-            </span>
+            <img className="house-calendar-toggle-image" src={dryerImage} alt="" aria-hidden="true" />
           </button>
         </div>
       </div>
