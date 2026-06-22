44d80b1755b8ce9ac78e00b4cc514f215d31fae9
asdfmnbvggg <inseojeong657@gmail.com>
2026-06-22 18:17:32 +0900

    feat : 코드 파일 정리

--- GPTHAR_alg/SmartHomeHARLib/SmartHomeHARLib/embedding/__init__.py
+++
@@ -1,6 +0,0 @@
-# Load main class & modules.
-
-from .base_embedding import BaseEmbedding
-from .word2vec import Word2VecEventEmbedder
-from .elmo import ELMoEventEmbedder
-from .embedding_visualisator import Visualisator
